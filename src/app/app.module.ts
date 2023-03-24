29cc0341afa5a3313ce8445f3055f42a36dd4c14
JJRoma <jj_roma@yahoo.ccom.ar>
2023-03-24 14:05:12 -0300

    nuevo porfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrincipalComponent } from './components/principal/principal.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ExperienciasComponent } from './components/experiencias/experiencias.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { CertificadosComponent } from './components/certificados/certificados.component';
+import { EstudiosComponent } from './components/estudios/estudios.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutComponent } from './components/about/about.component';
+import { HabilidadesComponent } from './components/habilidades/habilidades.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrincipalComponent,
+    HeaderComponent,
+    ExperienciasComponent,
+    ContactoComponent,
+    CertificadosComponent,
+    EstudiosComponent,
+    FooterComponent,
+    AboutComponent,
+    HabilidadesComponent
   ],
   imports: [
     BrowserModule,
